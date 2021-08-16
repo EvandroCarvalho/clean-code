bd96bb693ef5ca75ef69c1cb4422632e1cc4d590
EvandroCarvalho <32487329+EvandroCarvalho@users.noreply.github.com>
2021-08-16 00:56:46 -0300

    Update VacationPolicy.java

--- src/com/study/capitulo12/after/VacationPolicy.java
+++ src/com/study/capitulo12/after/VacationPolicy.java
@@ -2,10 +2,10 @@ package com.study.capitulo12.after;
 
 public class VacationPolicy {
 
-    public void accrueUSDDivisionVacation() {
+    public void accrueUSDivisionVacation() {
         // código para calcular as férias baseando-se nas horas trabalhadas de até a data
         // ...
-        // código para garantir que as férias cumpram o tempo minimo nos USD
+        // código para garantir que as férias cumpram o tempo minimo nos US
         // ...
         // código para aplicar férias ao registro de folha de pagamento
     }
@@ -13,7 +13,7 @@ public class VacationPolicy {
     public void accrueEUDivisionVacation() {
         // código para calcular as férias baseando-se nas horas trabalhadas de até a data
         // ...
-        // código para garantir que as férias cumpram o tempo minimo nos EUA
+        // código para garantir que as férias cumpram o tempo minimo nos EU
         // ...
         // código para aplicar férias ao registro de folha de pagamento
     }
